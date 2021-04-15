50e3c931ded87d37d87703e96c24330b5d6b06c1
suresh <sureshthota888@gmail.com>
2021-04-15 22:41:53 +0530

    ok

--- security/src/main/java/com/test/demo/service/SecurityConf.java
+++ security/src/main/java/com/test/demo/service/SecurityConf.java
@@ -1,4 +1,4 @@
-package com.test.demo.confi;
+package com.test.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +8,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-
-import com.test.demo.service.SecurityService;
 
 @EnableWebSecurity
 @Configuration
